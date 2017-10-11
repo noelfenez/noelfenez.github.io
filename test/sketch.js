00faaafe456d669e0af047f77a5706eb06ec360b
noelfenez <noelfenez@gmail.com>
2017-10-11 11:00:34 -0500

    numbers1

--- test/sketch.js
+++ test/sketch.js
@@ -16,12 +16,14 @@ function draw(){
 	beginShape();
 	for (var i = 0; i < touches.length; i++) {
 		vertex(touches[i].x, touches[i].y);
+		textSize(35);
+		text(touches[i].x,touches[i].x,touches[i].y);
+		text(touches[i].y,touches[i].x,touches[i].y);
 	}
 	
 	endShape(CLOSE);
 	fill(100, 220, 75);
 
-	
  for(var i=0;i<touches.length;i++){
   rect(touches[i].x,touches[i].y,150,150);
   }
