35ed1a8e773050fdb100c1fc03b4b0bbc4321367
noelfenez <noelfenez@gmail.com>
2017-10-11 09:48:36 -0500

    shape-test

--- test/sketch.js
+++ test/sketch.js
@@ -11,8 +11,19 @@ function setup() {
 }
 
 function draw(){
- background(255);
+ background(150);
  
+fill(200, 200, 50);
+	beginShape();
+	for (var i = 0; i < touches.length; i++) {
+		vertex(touches[i].x, touches[i].y);
+	}
+	endShape(CLOSE);
+	fill(175, 255, 100);
+	for (var j = 0; j < touches.length; j++) {
+		ellipse(touches[j].x, touches[j].y, 75, 75);
+	}
+	
  for(var i=0;i<touches.length;i++){
   rect(touches[i].x,touches[i].y,150,150);
   }
