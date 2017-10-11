e7fb13853873539d50d7e7188cb6bcbe9c6e4584
noelfenez <noelfenez@gmail.com>
2017-10-11 08:54:45 -0500

    test

---
+++ test/sketch.js
@@ -0,0 +1,19 @@
+document.ontouchmove = function(event){
+ event.preventDefault();
+}
+
+function setup() {
+ createCanvas(windowWidth,windowHeight);
+ rectMode(CENTER);
+ stroke(0);
+ noFill();
+ strokeWeight(5);
+}
+
+function draw(){
+ background(255);
+ 
+ for(var i=0;i<touches.length;i++){
+  rect(touches[i].x,touches[i].y,150,150);
+  }
+}
