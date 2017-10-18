fed14702aca496fb294c87db3065b15192080f54
noelfenez <noelfenez@gmail.com>
2017-10-18 09:56:42 -0500

    ball3

--- rotation/sketch.js
+++ rotation/sketch.js
@@ -1,34 +1,39 @@
-var px,py,vx,vy,ax,ay,vMultiplier;
+var px, py, vx, vy, ax, ay, vMultiplier;
 
 function setup() {
- createCanvas(windowWidth,windowHeight); 
+  createCanvas(windowWidth, windowHeight);
 
- vx = 0;
- vy = 0;
- ax = 0;
- ay = 0;
- vMultipier = 0.05;
- px = windowWidth/2;
- py = windowHeight/2;
+  vx = 0;
+  vy = 0;
+  ax = 0;
+  ay = 0;
+  vMultipier = 0.05;
+  px = windowWidth / 2;
+  py = windowHeight / 2;
 }
 
 function draw() {
- background(255);
- textSize(40);
- text("Rx: " + rotationX, 100, 100);
- text("Ry: " + rotationY, 100, 150);
- text("Rz: " + rotationZ, 100, 200);
- drawBall();
+  background(255);
+  textSize(40);
+  text("Rx: " + rotationX, 100, 100);
+  text("Ry: " + rotationY, 100, 150);
+  text("Rz: " + rotationZ, 100, 200);
+  drawBall();
 
 }
 
-function drawBall(){
-fill(70,100,600);
-ax = rotationY * vMultiplier;
-ay = rotationX * vMultiplier;
-vx += ax;
-vy += ay;
-px += vx;
-py += vy;
-ellipse(px,py,100)
+function drawBall() {
+  fill(70, 100, 60);
+  
+  ax = rotationY * vMultiplier;
+  ay = rotationX * vMultiplier;
+  
+  vx += ax;
+  vy += ay;
+  console.log(vx);
+   console.log(vy);
+  px += vx;
+  py += vy;
+  
+  ellipse(px, py, 100, 100);
 }
